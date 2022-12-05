e508c8c48065986e7bbbc76e657fd691b0052003
Ezequiel <oliverm316@gmail.com>
2022-12-05 13:44:56 +0100

    Implement Light Theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import Button from '@mui/material/Button'
 import './App.css'
 
 function App() {
   return (
     <div>
-      <h1 className='bg-red-500'>Chefs Awards</h1>
+      <h1 className='bg-red-500 mb-5'>Chefs Awards</h1>
+      <Button variant='contained'>Hello World</Button>
     </div>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { lightTheme } from './themes'
 import App from './App'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={lightTheme}>
+      <CssBaseline />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 )
