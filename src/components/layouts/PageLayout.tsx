8c558b43e6432af8ea6814ae120d6a36a2c26165
Ezequiel <oliverm316@gmail.com>
2022-12-13 17:12:03 +0100

    Main Layout with mosaics

--- src/components/layouts/PageLayout.tsx
+++ src/components/layouts/PageLayout.tsx
@@ -1,10 +1,11 @@
 import { Outlet } from 'react-router-dom'
 import { Box } from '@mui/material'
 import { Navbar } from '../molecules'
+import './layout.css'
 
 export const PageLayout = () => {
   return (
-    <Box className='flex flex-col min-h-screen bg-main bg-no-repeat fixed bg-center bg-contain'>
+    <Box className='flex flex-col min-h-screen bg-main bg-stretch'>
       <Navbar />
       <main className='my-[100px] mx-auto max-w-[1440px] px-[30px]'>
         <Outlet />
