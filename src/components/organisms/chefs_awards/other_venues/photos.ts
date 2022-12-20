5ad584a9e7bc920590f1508735d122e2b83c8d47
Ezequiel <oliverm316@gmail.com>
2022-12-20 18:09:59 +0100

    minor fix

--- src/components/organisms/chefs_awards/other_venues/photos.ts
+++ src/components/organisms/chefs_awards/other_venues/photos.ts
@@ -1,7 +1,7 @@
 import Aldamar from '../../../../assets/research/Aldamar.jpg'
 import Cartuja from '../../../../assets/research/cartuja.jpg'
 import Vallesa from '../../../../assets/research/vallesa.jpg'
-import Xamandreu from '../../../../assets/research/xamandreu.jpg'
+import Xamandreu from '../../../../assets/research/Xamandreu.jpg'
 
 export const venue_research = [
   {
