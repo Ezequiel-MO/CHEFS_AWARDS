1f993c6abbf6623a443556d1b075d62503626a08
Ezequiel <oliverm316@gmail.com>
2022-12-29 22:57:00 +0100

    updated budget

--- src/screens/detail/cf_budget/CF_table_data.ts
+++ src/screens/detail/cf_budget/CF_table_data.ts
@@ -9,45 +9,285 @@ export interface IBudget_data {
 export const tableDataCF: IBudget_data[] = [
   {
     category: 'Venue',
-    service: 'Dry Hire of Campo Anibal',
+    service:
+      'Dry Hire of Campo Anibal, min. 250 attendees, included in the food',
     quantity: 1,
-    unitPrice: 3000,
-    total: 3000
-  },
-  {
-    category: 'Food & Beverage',
-    service: 'Cocktails and Food',
-    quantity: 250,
-    unitPrice: 20,
-    total: 5000
+    unitPrice: 0,
+    total: 0
   },
   {
     category: 'Transportation',
-    service: 'Busses and Assistance',
+    service: '50 Seater Buses, 6hours at disposal, from 6pm-11pm',
     quantity: 6,
-    unitPrice: 1000,
-    total: 6000
+    unitPrice: 0,
+    total: 0
+  },
+  {
+    category: 'Transportation',
+    service: 'Eng. speaking hostess, 6hours service',
+    quantity: 6,
+    unitPrice: 196,
+    total: 1422.96
   },
   {
     category: 'Staffing',
-    service: 'Staff for the Event/Shows',
-    quantity: 10,
-    unitPrice: 1000,
-    total: 10000
+    service: 'Ladies dressed in traditional costumes to welcome guests',
+    quantity: 2,
+    unitPrice: 280,
+    total: 677.2
   },
   {
     category: 'Entertainment',
-    service: 'Show Elements',
+    service: 'Photo Call with Valencian Ladies',
     quantity: 1,
-    unitPrice: 10000,
-    total: 10000
+    unitPrice: 0,
+    total: 0
+  },
+  {
+    category: 'Gifts',
+    service: 'Traditional Valencian scarf for each guest',
+    quantity: 250,
+    unitPrice: 2.23,
+    total: 674.58
+  },
+  {
+    category: 'Gifts',
+    service: 'Optional customisation of the scarf with the logo',
+    quantity: 0,
+    unitPrice: 12,
+    total: 0
+  },
+  {
+    category: 'Gifts',
+    service: 'Optional small Paniculata bouquet',
+    quantity: 0,
+    unitPrice: 8,
+    total: 0
+  },
+  {
+    category: 'Decorations',
+    service: 'Red carpet main entrance - Cocktail area',
+    quantity: 1,
+    unitPrice: 0,
+    total: 0
+  },
+  {
+    category: 'Food & Beverage',
+    service: 'Welcome drink - 1 hour service w / snacks, from',
+    quantity: 250,
+    unitPrice: 11.5,
+    total: 3162.5
+  },
+  {
+    category: 'Entertainment',
+    service: 'Mascletà - Valencian Fireworks, from',
+    quantity: 1,
+    unitPrice: 2500,
+    total: 3025
+  },
+  {
+    category: 'Food & Beverage',
+    service:
+      'Standing Buffet, 20 canapes + 1 individual dish w/ drinks 7pm-9pm, from',
+    quantity: 250,
+    unitPrice: 47.5,
+    total: 13062.5
+  },
+  {
+    category: 'Food & Beverage',
+    service: 'Vieiras and Razor clams Station',
+    quantity: 250,
+    unitPrice: 3.75,
+    total: 1031.25
+  },
+  {
+    category: 'Food & Beverage',
+    service: 'Premium cheese Station',
+    quantity: 250,
+    unitPrice: 4.75,
+    total: 1306.25
+  },
+  {
+    category: 'Food & Beverage',
+    service: 'Huevos rotos - scrambled eggs Station',
+    quantity: 250,
+    unitPrice: 2.75,
+    total: 756.25
+  },
+  {
+    category: 'Food & Beverage',
+    service: 'Iberian Ham Carving Station - 2 Hams & 1 Ham Slicer',
+    quantity: 1,
+    unitPrice: 940,
+    total: 1034
+  },
+  {
+    category: 'Food & Beverage',
+    service: 'Chips and Croquettes Station',
+    quantity: 250,
+    unitPrice: 2.75,
+    total: 756.25
+  },
+  {
+    category: 'Food & Beverage',
+    service: 'Oysters Guillerdeau Station',
+    quantity: 250,
+    unitPrice: 5.75,
+    total: 1581.25
+  },
+  {
+    category: 'Food & Beverage',
+    service: 'Paella Show Cooking Station',
+    quantity: 1,
+    unitPrice: 325,
+    total: 357.5
+  },
+  {
+    category: 'Entertainment',
+    service: 'Pasacalles - Valencian Folkloric Parade with Band',
+    quantity: 1,
+    unitPrice: 325,
+    total: 357.5
+  },
+  {
+    category: 'Entertainment',
+    service: 'Moors Parade',
+    quantity: 1,
+    unitPrice: 0,
+    total: 0
+  },
+  {
+    category: 'Entertainment',
+    service: 'Moors and Christians Parade with Music Band',
+    quantity: 1,
+    unitPrice: 0,
+    total: 0
+  },
+  {
+    category: 'Entertainment',
+    service: 'Background Music',
+    quantity: 1,
+    unitPrice: 0,
+    total: 0
+  },
+  {
+    category: 'Entertainment',
+    service: 'Fireworks',
+    quantity: 1,
+    unitPrice: 3500,
+    total: 4235
+  },
+  {
+    category: 'Entertainment',
+    service: 'Falla Paper Mache Monument from',
+    quantity: 1,
+    unitPrice: 1650,
+    total: 1996.5
+  },
+  {
+    category: 'Food & Beverage',
+    service: 'Open Bar - 2 hours service, from',
+    quantity: 250,
+    unitPrice: 12,
+    total: 3300
+  },
+  {
+    category: 'Food & Beverage',
+    service: '(Optional - Cocktail Bar)',
+    quantity: 0,
+    unitPrice: 3,
+    total: 0
+  },
+  {
+    category: 'Entertainment',
+    service: 'DJ - 2 hours service, from',
+    quantity: 1,
+    unitPrice: 450,
+    total: 544.5
   },
   {
     category: 'AAVV',
-    service: 'Equipment and AAVV Staff',
+    service: 'Logo Projection on the façade of the Venue',
     quantity: 1,
-    unitPrice: 1000,
-    total: 1000
+    unitPrice: 0,
+    total: 0
+  },
+  {
+    category: 'AAVV',
+    service: 'Extra sound system for the cocktail area',
+    quantity: 1,
+    unitPrice: 0,
+    total: 0
+  },
+  {
+    category: 'AAVV',
+    service: 'Platform on the lake',
+    quantity: 1,
+    unitPrice: 0,
+    total: 0
+  },
+  {
+    category: 'AAVV',
+    service: 'Extra sound system for the Pergola area',
+    quantity: 1,
+    unitPrice: 0,
+    total: 0
+  },
+  {
+    category: 'AAVV',
+    service: 'Photographer and Videographer',
+    quantity: 1,
+    unitPrice: 700,
+    total: 847
+  },
+  {
+    category: 'Decorations',
+    service: '(Optional) Small Orange Tree',
+    quantity: 0,
+    unitPrice: 25,
+    total: 0
+  },
+  {
+    category: 'Decorations',
+    service: '(Optional) Small Olive Tree',
+    quantity: 0,
+    unitPrice: 11,
+    total: 0
+  },
+  {
+    category: 'Decorations',
+    service: '(Optional) Small Olive Tree and Customised Olive Oil',
+    quantity: 0,
+    unitPrice: 18,
+    total: 0
+  },
+  {
+    category: 'Gifts',
+    service: '(Optional) Rice Licour Bottle',
+    quantity: 0,
+    unitPrice: 4.5,
+    total: 0
+  },
+  {
+    category: 'Gifts',
+    service: '(Optional) Orange Licour Bottle',
+    quantity: 0,
+    unitPrice: 4,
+    total: 0
+  },
+  {
+    category: 'Gifts',
+    service: '(Optional) Bag of Rice',
+    quantity: 0,
+    unitPrice: 4.8,
+    total: 0
+  },
+  {
+    category: 'Staffing',
+    service: 'Onsite Event Manager ',
+    quantity: 2,
+    unitPrice: 400,
+    total: 968
   }
 ]
 
