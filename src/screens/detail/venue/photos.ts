5d1db5c01daeae56deca40d7c0311ae2c0c28eff
Ezequiel <oliverm316@gmail.com>
2022-12-31 12:23:11 +0100

    minor fix

--- src/screens/detail/venue/photos.ts
+++ src/screens/detail/venue/photos.ts
@@ -1,8 +1,8 @@
 import CFOVERVIEW from '../../../assets/chef_awards/birdviews/CFOverview.jpg'
 import CFPhotoCall from '../../../assets/chef_awards/birdviews/CFphotocall.jpg'
 import CFWelcomeDrink from '../../../assets/chef_awards/birdviews/CFWelcomeDrink.jpg'
-import CFDJ from '../../../assets/chef_awards/birdviews/CFdj.jpg'
-import CFMascleta from '../../../assets/chef_awards/birdviews/CFmascleta.jpg'
+import CFDJ from '../../../assets/chef_awards/birdviews/CFDJ.jpg'
+import CFMascleta from '../../../assets/chef_awards/birdviews/CFMascleta.jpg'
 
 export const bird_views = [
   {
