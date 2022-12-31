c02107ab92a3e74003fa78810301763d53ae02d1
Ezequiel <oliverm316@gmail.com>
2022-12-31 12:15:27 +0100

    minor change

--- src/screens/detail/venue/photos.ts
+++ src/screens/detail/venue/photos.ts
@@ -1,6 +1,6 @@
 import CFOVERVIEW from '../../../assets/chef_awards/birdviews/CFOverview.jpg'
 import CFPhotoCall from '../../../assets/chef_awards/birdviews/CFphotocall.jpg'
-import CFWelcomeDrink from '../../../assets/chef_awards/birdviews/CFwelcomedrink.jpg'
+import CFWelcomeDrink from '../../../assets/chef_awards/birdviews/CFwelcomeDrink.jpg'
 import CFDJ from '../../../assets/chef_awards/birdviews/CFdj.jpg'
 import CFMascleta from '../../../assets/chef_awards/birdviews/CFmascleta.jpg'
 
