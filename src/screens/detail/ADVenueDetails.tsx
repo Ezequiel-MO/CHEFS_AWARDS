c7f73c3ca0e1f8912f9b61e496bcb61ab37e516b
Ezequiel <oliverm316@gmail.com>
2023-01-05 14:23:43 +0100

    CAC video

--- src/screens/detail/ADVenueDetails.tsx
+++ src/screens/detail/ADVenueDetails.tsx
@@ -1,6 +1,6 @@
 import { Paper, Typography, Box } from '@mui/material'
 import { LocationWrapper } from '../../components/organisms'
-import Slide1 from '../../assets/hemisferic/slide1.png'
+import CACVideo from '../../assets/CAC.mp4'
 
 export const ADVenueDetails = () => {
   return (
@@ -8,25 +8,12 @@ export const ADVenueDetails = () => {
       <Typography variant='h1' className='bg-med_blue text-med_white p-2'>
         City Of Arts And Sciences
       </Typography>
-      <Box>
+      <Box className='my-5'>
         <Typography variant='h2'>Location</Typography>
         <LocationWrapper venue='CAC' />
       </Box>
-      <Box className='mt-5'>
-        <Typography variant='h2'>Red Carpet And Gala Dinner</Typography>
-      </Box>
       <Box>
-        <Typography variant='h2'>Photo Gallery</Typography>
-        <div className='flex flex-col items-center'>
-          <img src={Slide1} alt='slide1' />
-          <iframe
-            src='https://3dwarehouse.sketchup.com/ar-view/6f4250323a21285db01f5de0d4c60a8f'
-            width={850}
-            height={800}
-            title='3D View of Hemisferic'
-            className='bg-opacity-5'
-          />
-        </div>
+        <video src={CACVideo} autoPlay={true} controls={true} />
       </Box>
     </Paper>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 3600
+    chunkSizeWarningLimit: 36000
   }
 })
