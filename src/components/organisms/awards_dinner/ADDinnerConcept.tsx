73b9f28a0fcf446b068af841a9009693dff1c2b9
Ezequiel <oliverm316@gmail.com>
2023-01-05 16:04:05 +0100

    Text content for ADVenueDetails

--- src/components/organisms/awards_dinner/ADDinnerConcept.tsx
+++ src/components/organisms/awards_dinner/ADDinnerConcept.tsx
@@ -1,4 +1,3 @@
-import { Typography } from '@mui/material'
 import { PhotosLayout } from '../../layouts'
 import { AccordionParagraph } from '../../molecules'
 import { vanguard_photos } from './photos'
@@ -10,10 +9,26 @@ export const ADDinnerConcept = () => {
       id='panel2a-header'
       event='awards_dinner'
     >
-      <Typography paragraph variant='h6' align='left' className='indent-3'>
-        <strong>For this dinner</strong> we have been inspired by Valencia's
-        most vanguardist features
-      </Typography>
+      <p className='indent-5'>
+        For this dinner, we have been inspired by the most avant-garde elements
+        of the city, although following our common thread: the trencadis. In
+        contrast to the previous night, where the chefs tasted Valencian
+        gastronomy and we recreated a night of "Fallas", for this night we
+        thought we would show the most modern and avant-garde Valencia through
+        the City of Arts and Sciences.
+      </p>
+      <p className='indent-5 mb-5'>
+        It is the new symbol of the city and the image with which many people
+        around the world recognise it. Tonight, in addition to the chefs, the
+        protagonists will be the elements of nature and trencadis, which are the
+        elements that have inspired the construction of the CAC. These elements
+        of nature will be combined in different ways through the different
+        entertainment performances proposed. We will also give the avant-garde
+        touch with the projection of govos in the eye and the hologram of the
+        logo of the 50 restaurants (as an optional element) and the gastronomy
+        based on Nitro, without losing sight of the origins of Valencian
+        gastronomy.
+      </p>
 
       <PhotosLayout images={vanguard_photos} />
     </AccordionParagraph>
--- src/screens/detail/ADVenueDetails.tsx
+++ src/screens/detail/ADVenueDetails.tsx
@@ -10,6 +10,60 @@ export const ADVenueDetails = () => {
       </Typography>
       <Box className='cursor-pointer'>
         <video src={CACVideo} autoPlay={true} controls={true} />
+        <ol className='my-5'>
+          <li className='font-bold mb-5'>
+            Hemisferic : Red Carpet and Welcome Reception
+          </li>
+          <p className='italic indent-5'>
+            Being the "Oldest" of all the buildings in the{' '}
+            <strong>City of Arts and Sciences</strong> , the{' '}
+            <strong>Hemisferic</strong> is our{' '}
+            <strong className='underline'>
+              favorite choice location for the red carpet, welcome reception and
+              dinner
+            </strong>{' '}
+            , as opposed to the Principe Felipe Museum or Sala Arquerias or even
+            the Umbracle.
+          </p>
+          <br />
+          <p className='italic indent-5'>
+            Located between two lakes and overlooking the Umbracle and the Turia
+            Gardens, and its circular shape in the form of an eye, makes it
+            ideal for the reception of all attendees. The entrance to the eye
+            will be through the glass walkway, where we will place the red
+            carpet and registration area. Inside the Hemisferic, in the
+            perimeter ring, we will set up a platform in the north lake, to give
+            more space and make it easier for the attendees, especially in case
+            we have to take "covid" measures again. For the location of the
+            sponsors, we will design the floor in the form of trencadis, so that
+            each one has their own well-marked space or can even choose where to
+            be located. The sponsors will be interacting with different buffets
+            with avant-garde gastronomy and entertainment during the whole
+            reception. The Hemisferic has another advantage: the walls of the
+            space simulate the eyelid of an eye, so they can be opened or closed
+            depending on the weather conditions. With both sides open, and in
+            the shade, we will have natural fresh air from the Mediterranean
+            Sea, which is less than 1 km away.
+          </p>
+          <li className='font-bold mb-5'>
+            Palau de les Arts : Awards Ceremony
+          </li>
+          <p className='italic indent-5'>
+            At 8:00pm, we will announce that it is time to walk to the{' '}
+            <strong>Palau de les Arts</strong> , where, at the 11th floor, in
+            the Auditori, we will have the awards ceremony. We propose "Tania
+            Llasera as the night M.C, a well-known journalist and writer of
+            motivational books. In the <strong>Palau de les Arts</strong> we
+            will have a specially designed screen for the group, as well as a
+            live concert by the Palau Chamber Orchestra.
+          </p>
+          <li className='font-bold mb-5'>Umbracle : After Party</li>
+          <p className='italic indent-5'>
+            After the awards ceremony, we will have a grand party in the
+            <strong> Umbracle</strong> , where we will have drinks, a DJ and a
+            live music.
+          </p>
+        </ol>
         <div className='border border-med_brown '>
           <LocationWrapper venue='CAC' />
         </div>
