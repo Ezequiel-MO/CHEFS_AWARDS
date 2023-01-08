e6f2bf89b3c3d1ce8ec12ead65452c9ec3bc4ca0
Ezequiel <oliverm316@gmail.com>
2023-01-08 19:58:56 +0100

    updated slides for CAC

--- src/components/organisms/awards_dinner/ADTimeLineDescriptions.tsx
+++ src/components/organisms/awards_dinner/ADTimeLineDescriptions.tsx
@@ -1,5 +1,5 @@
-import { Box, Typography } from '@mui/material'
-import RedCarpet from '../../../assets/hemisferic/hemisferic_red_carpet.png'
+import { Box } from '@mui/material'
+import Hemisferic1 from '../../../assets/hemisferic/hemisferic1.png'
 import Hemisferic2 from '../../../assets/hemisferic/hemisferic2.png'
 import Hemisferic3 from '../../../assets/hemisferic/hemisferic3.png'
 import Hemisferic4 from '../../../assets/hemisferic/hemisferic4.png'
@@ -13,52 +13,54 @@ import Hemisferic11 from '../../../assets/hemisferic/hemisferic11.png'
 import Hemisferic12 from '../../../assets/hemisferic/hemisferic12.png'
 import Hemisferic13 from '../../../assets/hemisferic/hemisferic13.png'
 import Hemisferic14 from '../../../assets/hemisferic/hemisferic14.png'
-import Hemisferic15 from '../../../assets/hemisferic/hemisferic15.png'
-import Hemisferic16 from '../../../assets/hemisferic/hemisferic16.png'
-import Hemisferic17 from '../../../assets/hemisferic/hemisferic17.png'
-import Hemisferic18 from '../../../assets/hemisferic/hemisferic18.png'
-import Hemisferic19 from '../../../assets/hemisferic/hemisferic19.png'
 
 export const ADTimelineDescriptions = () => {
   return (
     <Box>
       <img
-        src={RedCarpet}
+        src={Hemisferic1}
         alt='red carpet'
-        id='da_arrivals'
         className='hover:scale-125 transition-transform hover:-ml-16'
+        loading='lazy'
       />
 
       <img
         src={Hemisferic2}
         alt='hemisferic_2'
-        id='da_welcome_drink'
+        id='da_red_carpet'
         className='hover:scale-125 transition-transform hover:-ml-16'
+        loading='lazy'
       />
       <img
         src={Hemisferic3}
         alt='hemisferic_3'
         className='hover:scale-125 transition-transform hover:-ml-16'
+        loading='lazy'
       />
       <img
         src={Hemisferic4}
         alt='hemisferic_4'
         className='hover:scale-125 transition-transform hover:-ml-16'
+        loading='lazy'
       />
       <img
         src={Hemisferic5}
         alt='hemisferic_5'
         className='hover:scale-125 transition-transform hover:-ml-16'
+        loading='lazy'
+        id='da_cocktail'
       />
       <img
         src={Hemisferic6}
         alt='hemisferic_6'
         className='hover:scale-125 transition-transform hover:-ml-16'
+        loading='lazy'
       />
       <img
         src={Hemisferic7}
         alt='hemisferic_7'
         className='hover:scale-125 transition-transform hover:-ml-16'
+        loading='lazy'
       />
 
       <img
@@ -66,75 +68,46 @@ export const ADTimelineDescriptions = () => {
         alt='hemisferic_8'
         id='da_dinner'
         className='hover:scale-125 transition-transform hover:-ml-16'
+        loading='lazy'
       />
       <img
+        id='da_walk'
         src={Hemisferic9}
         alt='hemisferic_9'
         className='hover:scale-125 transition-transform hover:-ml-16'
+        loading='lazy'
       />
       <img
         src={Hemisferic10}
         alt='hemisferic_10'
         className='hover:scale-125 transition-transform hover:-ml-16'
+        loading='lazy'
       />
       <img
         src={Hemisferic11}
         alt='hemisferic_11'
         className='hover:scale-125 transition-transform hover:-ml-16'
+        loading='lazy'
       />
       <img
+        id='da_party'
         src={Hemisferic12}
         alt='hemisferic_12'
         className='hover:scale-125 transition-transform hover:-ml-16'
+        loading='lazy'
       />
       <img
         src={Hemisferic13}
         alt='hemisferic_13'
         className='hover:scale-125 transition-transform hover:-ml-16'
+        loading='lazy'
       />
       <img
         src={Hemisferic14}
         alt='hemisferic_14'
         className='hover:scale-125 transition-transform hover:-ml-16'
+        loading='lazy'
       />
-      <img
-        src={Hemisferic15}
-        alt='hemisferic_15'
-        className='hover:scale-125 transition-transform hover:-ml-16'
-      />
-      <img
-        src={Hemisferic16}
-        alt='hemisferic_16'
-        className='hover:scale-125 transition-transform hover:-ml-16'
-      />
-      <img
-        src={Hemisferic17}
-        alt='hemisferic_17'
-        className='hover:scale-125 transition-transform hover:-ml-16'
-      />
-      <img
-        src={Hemisferic18}
-        alt='hemisferic_18'
-        className='hover:scale-125 transition-transform hover:-ml-16'
-      />
-      <img
-        src={Hemisferic19}
-        alt='hemisferic_19'
-        className='hover:scale-125 transition-transform hover:-ml-16'
-      />
-
-      <Typography
-        variant='h2'
-        sx={{ mb: 1, height: '10vh' }}
-        className='text-med_purple'
-        id='da_party'
-      >
-        Dance Party
-        <br />
-        <br />
-        11pm to 1am – Party in Umbracle in exclusivity for the event ​ 1am –
-        Back to hotels at leisure
-      </Typography>
     </Box>
   )
 }
--- src/screens/detail/timeline_data.ts
+++ src/screens/detail/timeline_data.ts
@@ -27,27 +27,27 @@ export const chef_feast_timeline_data = [
 
 export const dinner_awards_timeline_data = [
   {
-    id: 'da_arrivals',
-    time: '06:00 pm',
-    icon: 'material-symbols:directions-bus-outline',
-    title: 'Arrivals'
-  },
-  {
-    id: 'da_welcome_drink',
+    id: 'da_red_carpet',
     time: '6-8 pm',
     icon: 'game-icons:red-carpet',
-    title: 'Red Carpet'
+    title: 'Red Carpet & Registration'
   },
   {
-    id: 'da_dinner',
+    id: 'da_cocktail',
     time: '6-8 pm',
     icon: 'cil:dinner',
-    title: 'Gala Dinner'
+    title: 'Cocktail'
+  },
+  {
+    id: 'da_walk',
+    time: '8-8:45',
+    icon: 'cil:dinner',
+    title: 'Walk to Palau'
   },
   {
     id: 'da_party',
-    time: '8-8:45',
+    time: '8:45-10:00',
     icon: 'material-symbols:directions-walk',
-    title: 'Walk to Palau de les Arts'
+    title: 'Awards Ceremony'
   }
 ]
